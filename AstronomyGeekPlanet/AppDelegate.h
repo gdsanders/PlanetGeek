7083852adef8eecb1bf6ba2756ebdbb915399f1a
gdsanders <gd@digitalequity.com>
2015-09-14 21:29:04 -0500

    Initial Commit

---
+++ AstronomyGeekPlanet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AstronomyGeekPlanet
+//
+//  Created by G.D. Sanders on 9/14/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AstronomyGeekPlanet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AstronomyGeekPlanet
+//
+//  Created by G.D. Sanders on 9/14/15.
+//  Copyright © 2015 DigitalEquity, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
